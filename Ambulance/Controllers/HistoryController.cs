2ee5209257e611e38f3d325541bdbc97c6210ed4
yury <jur4ik2008@yandex.ru>
2016-01-04 23:48:32 +0300

    Init

---
+++ Ambulance/Controllers/HistoryController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ambulance.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private ambulanceEntities db = new ambulanceEntities();
+
+        //
+        // GET: /History/
+
+        public ActionResult Index()
+        {
+            var ill_history = db.ill_history.Include(i => i.doctors).Include(i => i.palata);
+            return View(ill_history.ToList());
+        }
+
+        //
+        // GET: /History/Details/5
+
+        public ActionResult Details(long id = 0)
+        {
+            ill_history ill_history = db.ill_history.Find(id);
+            if (ill_history == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ill_history);
+        }
+
+        //
+        // GET: /History/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.shifr = new SelectList(db.doctors, "shifr", "d_name");
+            ViewBag.Pal_id = new SelectList(db.palata, "Pal_id", "Pal_id");
+            return View();
+        }
+
+        //
+        // POST: /History/Create
+
+        [HttpPost]
+        public ActionResult Create(ill_history ill_history)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ill_history.Add(ill_history);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.shifr = new SelectList(db.doctors, "shifr", "d_name", ill_history.shifr);
+            ViewBag.Pal_id = new SelectList(db.palata, "Pal_id", "Pal_id", ill_history.Pal_id);
+            return View(ill_history);
+        }
+
+        //
+        // GET: /History/Edit/5
+
+        public ActionResult Edit(long id = 0)
+        {
+            ill_history ill_history = db.ill_history.Find(id);
+            if (ill_history == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.shifr = new SelectList(db.doctors, "shifr", "d_name", ill_history.shifr);
+            ViewBag.Pal_id = new SelectList(db.palata, "Pal_id", "Pal_id", ill_history.Pal_id);
+            return View(ill_history);
+        }
+
+        //
+        // POST: /History/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(ill_history ill_history)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ill_history).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.shifr = new SelectList(db.doctors, "shifr", "d_name", ill_history.shifr);
+            ViewBag.Pal_id = new SelectList(db.palata, "Pal_id", "Pal_id", ill_history.Pal_id);
+            return View(ill_history);
+        }
+
+        //
+        // GET: /History/Delete/5
+
+        public ActionResult Delete(long id = 0)
+        {
+            ill_history ill_history = db.ill_history.Find(id);
+            if (ill_history == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ill_history);
+        }
+
+        //
+        // POST: /History/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            ill_history ill_history = db.ill_history.Find(id);
+            db.ill_history.Remove(ill_history);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Ambulance/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Ambulance.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(ambulance_user user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (ambulanceEntities dc = new ambulanceEntities())
+                {
+                    string cypher;
+                    using (MD5 md5Hash = MD5.Create())
+                        {
+                             cypher = GetMd5Hash(md5Hash,user.password.ToString());
+ 
+                        }
+                    ambulance_user v = dc.ambulance_user.Where(a => a.login.Equals(user.login) && a.password.Equals(cypher)).FirstOrDefault();
+                    
+                    
+                    if (v != null)
+                    {
+                       
+                        Session["LogedUserId"] = v.id.ToString();
+                        Session["LogedUserLogin"] = v.login.ToString();
+                        Session["LogedUserRole"] = v.role_id.ToString();
+                        Session["LogedUserPass"] = cypher;
+                        return RedirectToAction("AfterLogin");
+                    }
+                }
+            }
+            return View(user);
+        }
+        public ActionResult AfterLogin()
+        {
+            //if (Session["LogedUserId"] != null)
+            //{
+                return View();
+            //}
+            //else
+            //{
+            //   return RedirectToAction("Index");
+            //}
+            
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+    }
+        }
+}
